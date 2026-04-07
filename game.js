d05fce10b1ba5086310614a9651a8efdf8769d27
picturecube <152343360+picturecube@users.noreply.github.com>
2026-04-06 20:37:24 -0400

    separated world bounds into a group of 4 sides so it actually works

--- game.js
+++ game.js
@@ -17,10 +17,16 @@ class GameWindow extends Phaser.Scene
     create () {
 
         this.ground = this.add.image(400, 300, 'ground');
+
         this.player = new Player(this, 400, 300, 'player');
-        this.bounds = this.physics.add.staticImage(400, 300, 'bounds');
-        this.bounds.setScale(10);
-        this.bounds.refreshBody();
+
+        this.bounds = this.physics.add.staticGroup();
+
+        this.bounds.create(-20, 2500, 'bounds');
+        this.bounds.create(5020, 2500, 'bounds');
+        this.bounds.create(2500, -20, 'bounds');
+        this.bounds.create(2500, 5020, 'bounds');
+
         this.physics.add.collider(this.player, this.bounds);
 
         this.cameras.main.setBackgroundColor('#2e2e2e');
