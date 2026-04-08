e8e333a321a7fc1541fa3ff831aa0ef5e55b7792
picturecube <152343360+picturecube@users.noreply.github.com>
2026-04-07 21:20:01 -0400

    changed camera angle

--- player.js
+++ player.js
@@ -67,7 +67,7 @@ class Player extends Phaser.Physics.Arcade.Image {
         this.setVelocityForward(this.velocityForward);
 
         this.scene.cameras.main.setScroll(this.x - 400, this.y - 300); //moves the camera to follow the player
-        this.scene.cameras.main.setAngle(180 - this.angle); //rotates the camera to match the player's rotation
+        this.scene.cameras.main.setAngle(270 - this.angle); //rotates the camera to match the player's rotation
 
     };
 
