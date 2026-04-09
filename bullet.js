1792779c01670fed2a262fee41910487c13c6925
picturecube <152343360+picturecube@users.noreply.github.com>
2026-04-09 17:34:46 -0400

    made bullet fire

---
+++ bullet.js
@@ -0,0 +1,27 @@
+class Bullet extends Phaser.Physics.Arcade.Group {
+    constructor(scene, player) {
+
+        super(scene, player.x, player.y, 'bullet');
+
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        this.setVisible(false);
+
+    }
+
+    fire(x, y, playerX, playerY) {
+
+        this.setVisible(true);
+
+        this.moveTo(this, x, y, 150);
+
+        if (sqrt((playerX - x) ** 2 + (playerY - y) ** 2) < 500) {
+
+            this.destroy();
+
+        };
+
+    };
+
+};
--- game.js
+++ game.js
@@ -1,10 +1,7 @@
 var ground;
 var player;
-var bounds;
-var leftBound;
-var rightBound;
-var topBound;
-var bottomBound;
+var bullet;
+var pointer;
 
 class GameWindow extends Phaser.Scene
 {
@@ -13,7 +10,7 @@ class GameWindow extends Phaser.Scene
 
     preload () {
 
-        this.load.image('ground', 'assets/ground.png'); //preload assets
+        this.load.image('ground', 'assets/ground.png'); // preload assets
         this.load.image('player', 'assets/player.png');
         this.load.image('bullet', 'assets/bullet.png');
 
@@ -21,21 +18,36 @@ class GameWindow extends Phaser.Scene
 
     create () {
 
-        ground = this.add.image(400, 300, 'ground'); //adds a ground first
+        ground = this.add.image(400, 300, 'ground'); // adds a ground first
 
-        player = new Player(this, 400, 300, 'player'); //adds player
+        
+        player = new Player(this, 400, 300, 'player'); // adds player
 
-        this.physics.world.setBounds(-2100, -2200, 5000, 5000, true, true, true, true); //sets world bounds
+        bullets = [new Bullet(this, player)]; // adds a bullet
 
-        this.cameras.main.setBackgroundColor('#2e2e2e'); //sets background color to a dark gray
+        pointer = screen.input.activePointer; // gets the pointer
 
-        player.initialize(); //calls the player's initialize function
+        this.physics.world.setBounds(-2100, -2200, 5000, 5000, true, true, true, true); // sets world bounds
+
+        this.cameras.main.setBackgroundColor('#2e2e2e'); // sets background color to a dark gray
+
+        player.initialize(); // calls the player's initialize function
 
     };
 
     update () {
 
-        player.update(); //calls the player's update function
+        player.update(); // calls the player's update function
+
+        if (pointer.isDown) { // if the pointer is down, fire the bullet
+
+            bullets[-1].fire(pointer.worldX, pointer.worldY, player.x, player.y); // fires the last bullet in the array towards the pointer's world coordinates
+            
+            bullets.filter(bullet => bullet.active); // filters out inactive bullets from the array
+            bullets.push(new Bullet(this, player)); // adds a new bullet to the array
+
+        };
 
     };
+
 };
