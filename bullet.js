3c5fef57e2ed569fae61c3c9ab08a0e7e5bfc9d1
picturecube <152343360+picturecube@users.noreply.github.com>
2026-04-10 11:13:49 -0400

    reversed bullet velocity and rotation

--- bullet.js
+++ bullet.js
@@ -18,8 +18,8 @@ class Bullet extends Phaser.Physics.Arcade.Image {
 
         this.status = 'fired'; // sets the status of the bullet to "fired" to indicate that it is currently being fired
 
-        this.rotation = Phaser.Math.Angle.Between(playerX, playerY, x, y); // makes the bullet face the mouse
-        this.setVelocityForward(-500); // sets the velocity to 150 pixels per second in the direction the player is facing
+        this.rotation = 0 - Phaser.Math.Angle.Between(playerX, playerY, x, y); // makes the bullet face the mouse
+        this.setVelocityForward(500); // sets the velocity to 500 pixels per second in the direction the player is facing
 
         this.sourceX = playerX; // stores the player's x position in a variable for later use
         this.sourceY = playerY; // stores the player's y position in a variable for later use
