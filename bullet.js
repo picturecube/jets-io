cdd43fa2319073b7dd9394de468bc7fbd67376b9
picturecube <152343360+picturecube@users.noreply.github.com>
2026-04-12 20:05:34 -0400

    debug

--- bullet.js
+++ bullet.js
@@ -27,6 +27,8 @@ class Bullet extends Phaser.Physics.Arcade.Image {
             this.setVisible(true);
     
             this.status = 'fired'; // sets the status of the bullet to "fired" to indicate that it is currently being fired
+
+            console.log(x, y);
     
             this.rotation = Math.PI + Phaser.Math.Angle.Between(this.source.x, this.source.y, x, y); // makes the bullet face the mouse
             this.setVelocityForward(/**this.calculatePlayerVelocityRelativeToMouse() - **/ 0 - 500); // sets the velocity to 500 pixels per second in the direction the player is facing
